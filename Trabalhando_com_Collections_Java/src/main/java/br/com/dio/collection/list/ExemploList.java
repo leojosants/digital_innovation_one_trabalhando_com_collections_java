4794a8a3ee4af8a122bb22a344514021fad47eb4
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-25 14:27:09 -0300

    Conhecendo os métodos List - Parte 2

--- Trabalhando_com_Collections_Java/src/main/java/br/com/dio/collection/list/ExemploList.java
+++ Trabalhando_com_Collections_Java/src/main/java/br/com/dio/collection/list/ExemploList.java
@@ -1,8 +1,6 @@
 package br.com.dio.collection.list;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
     public static void main(String[] args) {
@@ -62,6 +60,89 @@ public class ExemploList {
         for (Double nota: notas) {
             System.out.println(nota);
         }
+
+        System.out.println("\nExiba a terceira nota adicionada:");
+        System.out.println(notas);
+        System.out.println(notas.get(2));
+
+        System.out.println("\nExiba a menor nota:");
+        System.out.println(notas);
+        System.out.println(Collections.min(notas));
+
+        System.out.println("\nExiba a maior nota:");
+        System.out.println(notas);
+        System.out.println(Collections.max(notas));
+
+        System.out.println("\nExiba a soma dos valores: ");
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0.0;
+        while (iterator.hasNext()) {
+            Double next = iterator.next();
+            soma += next;
+        }
+        System.out.println(soma);
+
+        System.out.println("\nExiba a média das notas:");
+        Double media = soma / notas.size();
+        System.out.println(media);
+
+        System.out.println("\nRemova a nota 0.0");
+        System.out.println(notas);
+        System.out.println(notas.remove(0.0));
+        System.out.println(notas);
+
+        System.out.println("\nRemova a nota na posição 0");
+        System.out.println(notas);
+        System.out.println(notas.remove(0));
+        System.out.println(notas);
+
+        System.out.println("\nRemova as notas menores que 7 e exiba a lista:");
+        System.out.println(notas);
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()) {
+            Double next = iterator1.next();
+            if (next < 7) {
+                iterator1.remove();
+            }
+        }
+        System.out.println(notas);
+
+        System.out.println("\nApague toda a lista.");
+        System.out.println(notas);
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("\nConfira se a lista está vazia.");
+        System.out.println(notas);
+        System.out.println(notas.isEmpty());
+
+        // Resolva esses exercícios utilizando os métodos da implementação LinkedList:
+        System.out.println("\nCrie uma lista chamada notas2 e coloque todos os elementos da List ArrayList nessa nova lista.");
+        List<Double> notas2 = new LinkedList<Double>(); // Generics(jdk 5)
+        System.out.println(notas2.size());
+        notas2.add(7.0);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5.0);
+        notas2.add(7.0);
+        notas2.add(0.0);
+        notas2.add(3.6);
+        System.out.println(notas2);
+
+        System.out.println("\nMostre a primeira nota da nova lista sem removê-lo:");
+        System.out.println(notas2.get(0));
+        System.out.println(notas2);
+
+        System.out.println("\nMostre a primeira nota da nova lista removendo-o:");
+        System.out.println(notas2.get(0));
+        notas2.remove(0);
+        System.out.println(notas2);
+
+//        System.out.println("\n");
+//        System.out.println("\n");
+//        System.out.println("\n");
+//        System.out.println("\n");
+//        System.out.println("\n");
 
 
     }
